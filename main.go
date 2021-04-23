855a349a82de80208440187c6ca620f96b0b5da5
christophberger <christophberger@users.noreply.github.com>
2021-04-23 21:11:47 +0200

    feat: Add version information

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"runtime/debug"
 )
 
 func main() {
 	fmt.Println("goreleaser test")
+	if info, ok := debug.ReadBuildInfo(); ok {
+		fmt.Println("Version", info.Main.Version)
+	}
 }
